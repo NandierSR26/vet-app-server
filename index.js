b8f659f296345beaa94a8ed265554b10eb57089c
Nandier Ruiz <nandiersruiz@outlook.com>
2022-11-24 21:06:41 -0500

    index.js updates

--- index.js
+++ index.js
@@ -12,19 +12,6 @@ dotenv.config();
 
 conectarDB();
 
-// const dominiosPermitidos = [process.env.FRONTEND_URL, 'http://localhost:3000'];
-
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     if (dominiosPermitidos.indexOf(origin) !== -1) {
-//       // El Origen del Request esta permitido
-//       callback(null, true);
-//     } else {
-//       callback(new Error("No permitido por CORS"));
-//     }
-//   },
-// };
-
 app.use(express.static("public"));
 
 app.use(cors());
